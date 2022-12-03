8e30d1f536683906a1b33d0c2f5c4d134a9deaeb
Soapboxes <furmaks@gmail.com>
2022-12-04 00:30:51 +0300

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Info {
     public static void main (String[] args) {
-        System.out.println("04.12.2022");
+        System.out.println("04.12.22");
 
     }
 }
