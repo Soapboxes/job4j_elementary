6dc31e175b256a4ed4c436d3b08e607ac871c9d6
Soapboxes <furmaks@gmail.com>
2022-12-22 17:13:06 +0300

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -4,5 +4,11 @@ public class Info {
     public static void main(String[] args) {
         System.out.println("04.12.22");
 
+        int age = 18;
+        System.out.println(age);
+        float length = 78.0332f;
+        System.out.println(length);
+        boolean baby = true;
+        System.out.println(baby);
     }
 }
