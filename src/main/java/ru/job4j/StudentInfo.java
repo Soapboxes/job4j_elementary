c5f22ccd65c9abf83825c59135ae1b4be9496776
Soapboxes <furmaks@gmail.com>
2023-09-16 12:41:35 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Maksim F");
+        System.out.println("20.01.1990");
+
+    }
+}
