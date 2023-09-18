231dfd5166d9a889904021ba1941ec26cd321b89
Soapboxes <furmaks@gmail.com>
2023-09-18 19:52:22 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/condition/TrgArea.java
+++ src/main/java/ru/job4j/condition/TrgArea.java
@@ -4,7 +4,7 @@ public class TrgArea {
     public static double area(double a, double b, double c) {
         double sumP = (a + b + c) / 2;
         return Math.sqrt(sumP * (sumP - a) * (sumP - b) * (sumP - c));
-        //return Math.sqrt(((a + b + c) / 2) * (((a + b + c) / 2) - a) * (((a + b + c) / 2) - b) * (((a + b + c) / 2) - c));
+        //return Math.sqrt((a + b + c) / 2) * (((a + b + c) / 2) - a) * (((a + b + c) / 2) - b) * (((a + b + c) / 2) - c);
     }
 
     public static void main(String[] args) {
