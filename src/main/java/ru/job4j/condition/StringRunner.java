2190974f88d0e7d0e044a90c7ee0d769a29f33ce
Soapboxes <furmaks@gmail.com>
2023-09-20 14:29:52 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/condition/StringRunner.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class StringRunner {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        int year = 2023;
+        idea += " But I am a newbie. ";
+        idea += year;
+        System.out.println(idea);
+    }
+}
