50046abfd8ffae1fc84de830fb4b83aeb5d4f54e
Soapboxes <furmaks@gmail.com>
2023-09-20 17:12:46 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,35 @@
+package ru.job4j.converter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    void whenConvert140RblThen2Euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void whenConvert140RblThen2dot3333Dollars() {
+        float in = 140;
+        float expected = 2.3333333f;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+
+    @Test
+    void whenConvert140RblThen10dot5263Yuan() {
+        float in = 140;
+        float expected = 10.526316f;
+        float out = Converter.rubleToYuan(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+}
