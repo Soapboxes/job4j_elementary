40986515f4d3bf0e8876c38a86465c49c391163f
Soapboxes <furmaks@gmail.com>
2023-09-20 15:56:40 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -3,18 +3,15 @@ package ru.job4j.converter;
 public class Converter {
 
     public static float rubleToEuro(float value) {
-        float rsl = value / 70;
-        return rsl;
+        return value / 70;
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value / 60;
-        return rsl;
+        return value / 60;
     }
 
     public static float rubleToYuan(float value) {
-        float rsl = value / 13.3f;
-        return rsl;
+        return value / 13.3f;
     }
 
     public static void main(String[] args) {
@@ -24,5 +21,19 @@ public class Converter {
         System.out.println("140 rubles are " + dollar + " dollars.");
         float rubleToYuan = Converter.rubleToYuan(140);
         System.out.println("140 rubles are " + rubleToYuan + " yuan.");
-            }
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        float expectedDol = 2.3333333f;
+        float outDol = Converter.rubleToDollar(in);
+        boolean passedDol = expectedDol == outDol;
+        System.out.println("140 rubles are 2,3333333. Test result : " + passedDol);
+        float expectedYuan = 10.526316f;
+        float outYuan = Converter.rubleToYuan(in);
+        boolean passedYuan = expectedYuan == outYuan;
+        System.out.println("140 rubles are 10,526316. Test result : " + passedYuan);
+
+    }
 }
