aede37c5236aff4035d80810149421f50a8263b4
Soapboxes <furmaks@gmail.com>
2023-09-24 13:25:45 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,5 +1,7 @@
 package ru.job4j.condition;
 
+import java.text.DecimalFormat;
+
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
         /*
@@ -13,9 +15,15 @@ public class Point {
     }
 
     public static void main(String[] args) {
+        DecimalFormat dF = new DecimalFormat("##.###");
         double result = Point.distance(0, 0, 2, 0);
         System.out.println("result (0,0) to (2, 0) " + result);
         double resultTwo = Point.distance(1, 2, 2, 1);
-        System.out.println("result (1, 2) to (2, 1) " + resultTwo);
+        System.out.println("result (1, 2) to (2, 1) " + dF.format(resultTwo));
+        double resultThree = Point.distance(1, 0, 2, 2);
+        System.out.println("result (1, 0) to (2, 2) " + dF.format(resultThree));
+        double resultFour = Point.distance(10, 0, 2, 0);
+        System.out.println("result (10, 0) to (2, 2) " + dF.format(resultFour));
+
     }
 }
