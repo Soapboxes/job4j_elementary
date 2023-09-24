6e8c65783a9f2372c412535055be5f8f683a3713
Soapboxes <furmaks@gmail.com>
2023-09-24 12:17:22 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -2,21 +2,22 @@ package ru.job4j.calculator;
 
 public class Fit {
 
-    public static double manWeight(double height) {
-        double rsl = (height - 100) * 1.15;
+    public static double manWeight(double heightM) {
+        double rsl = (heightM - 100) * 1.15;
         return rsl;
     }
 
-    public static double womanWeight(double height) {
-        double rsl = (height - 110) * 1.15;
+    public static double womanWeight(double heightW) {
+        double rsl = (heightW - 110) * 1.15;
         return rsl;
     }
 
     public static void main(String[] args) {
-        short height = 187;
-        double man = Fit.manWeight(height);
-        double woman = Fit.womanWeight(height);
-        System.out.println("Man 187 is " + man);
-        System.out.println("Woman 187 is " + woman);
+        short heightM = 180;
+        short heightW = 170;
+        double man = Fit.manWeight(heightM);
+        double woman = Fit.womanWeight(heightW);
+        System.out.println("Man 180 is " + man);
+        System.out.println("Woman 170 is " + woman);
     }
 }
