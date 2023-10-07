91105404a7de04b3b9285c4e850330ad599d9885
Soapboxes <furmaks@gmail.com>
2023-10-07 21:15:28 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,7 +3,7 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int number) {
         boolean prime = false;
-        for (int ind = 2; ind <= number; ind++) {
+        for (int ind = 0; ind <= number; ind++) {
             if ((number > 2) && (number % number == 0) && (number % 2 == 1)) {
                 prime = true;
                 break;
