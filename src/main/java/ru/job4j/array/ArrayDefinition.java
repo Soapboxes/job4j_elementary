62ed0ca9e1e25864fcf31ef6d722d285c32ece6d
Soapboxes <furmaks@gmail.com>
2023-10-10 19:28:13 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,13 @@ public class ArrayDefinition {
         Short[] ages = new Short[10];
         String[] surnames = new String[100500];
         Float[] prices = new Float[40];
+        String[] names = new String[4];
+        names[0] = "strong";
+        names[1] = "hard";
+        names[2] = " massiv";
+        names[3] = "work";
+        System.out.println("Массивы names: " + names[0] + " " + names[1] + " " + names[2] + " " + names[3]);
+        System.out.println(names[3]);
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
