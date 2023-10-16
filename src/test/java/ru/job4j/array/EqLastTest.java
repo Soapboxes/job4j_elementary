d49f92dc9c9f7af4e1857139884f215ae3c3cacb
Soapboxes <furmaks@gmail.com>
2023-10-16 22:27:37 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/test/java/ru/job4j/array/EqLastTest.java
@@ -0,0 +1,39 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EqLastTest {
+    @Test
+    public void whenEq() {
+        int[] left = {1, 2, 3};
+        int[] right = {5, 4, 3};
+        boolean result = EqLast.check(left, right);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenNotEq() {
+        int[] left = {1, 2, 3};
+        int[] right = {3, 3, 4};
+        boolean result = EqLast.check(left, right);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void whenEq2() {
+        int[] left = {1, 2, 3, 4};
+        int[] right = {1, 2, 3, 4};
+        boolean result = EqLast.check(left, right);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenNotEq2() {
+        int[] left = {3, 3, 4};
+        int[] right = {3, 3, 4, 5};
+        boolean result = EqLast.check(left, right);
+        assertThat(result).isFalse();
+    }
+}
