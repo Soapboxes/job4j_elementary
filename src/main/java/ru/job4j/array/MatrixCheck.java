ee1ec5a8ba5a9b24123ffa0e5390cc0ce2801a0e
Soapboxes <furmaks@gmail.com>
2023-10-23 20:34:03 +0300

    3.3. Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -0,0 +1,15 @@
+package ru.job4j.array;
+
+public class MatrixCheck {
+    public static boolean monoHorizontal(char[][] board, int row) {
+        boolean result = true;
+        for (int ind = 0; ind < board.length; ind++) {
+                if (board[row][ind] == ' ') {
+                    result = false;
+                    break;
+                }
+            }
+        return result;
+
+    }
+}
