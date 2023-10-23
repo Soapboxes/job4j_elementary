cc935c690a6c8b5f479622fcf1e344726220770b
Soapboxes <furmaks@gmail.com>
2023-10-23 15:29:24 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/array/SkipNegative.java
+++ src/main/java/ru/job4j/array/SkipNegative.java
@@ -4,8 +4,7 @@ public class SkipNegative {
     public static int[][] skip(int[][] array) {
         for (int row = 0; row < array.length; row++) {
             for (int cell = 0; cell < array[row].length; cell++) {
-                int rsl = array[row][cell];
-                if (rsl < 0) {
+                if (array[row][cell] < 0) {
                     array[row][cell] = 0;
                 }
             }
