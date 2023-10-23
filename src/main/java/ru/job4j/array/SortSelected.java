efb987c760b18925aeb57e70b957f6029212650b
Soapboxes <furmaks@gmail.com>
2023-10-23 14:18:07 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class SortSelected {
     public static int[] sort(int[] data) {
-        for (int j = 0; j <= data.length - 1; j++) {
+        for (int j = 0; j < data.length; j++) {
             int min = MinDiapason.findMin(data, j, data.length - 1);
             int index = FindLoop.indexInRange(data, min, j, data.length - 1);
             SwitchArray.swap(data, j, index);
