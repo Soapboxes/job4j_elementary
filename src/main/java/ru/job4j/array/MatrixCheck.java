5601b0e2ac43309525ce117733335700063f80e4
Soapboxes <furmaks@gmail.com>
2023-10-30 12:06:30 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -35,7 +35,7 @@ public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
         for (int ind = 0; ind < board.length; ind++) {
-            if (board[ind][ind] == 'X' && monoHorizontal(board, ind) || monoVertical(board, ind)) {
+            if (board[ind][ind] == 'X' && (monoHorizontal(board, ind) || monoVertical(board, ind))) {
                 result = true;
                 break;
             }
