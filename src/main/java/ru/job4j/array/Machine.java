61995420ca709ce6359954710b77dd43bbb21f82
Soapboxes <furmaks@gmail.com>
2023-10-30 13:10:20 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -7,11 +7,10 @@ public class Machine {
         int[] coins = {10, 5, 2, 1};
         int[] rsl = new int[100];
         int size = 0;
-        int sum = money - price;
-        for (int i = 0; i < coins.length; i++) {
-            while (sum >= coins[i]) {
-                sum -= coins[i];
-                rsl[size] = coins[i];
+        for (int coin : coins) {
+            while (money - price >= coin) {
+                money -= coin;
+                rsl[size] = coin;
                 size++;
             }
         }
