51b58be06a4f1098db5ab5d04a7538e5f6cf4858
Soapboxes <furmaks@gmail.com>
2023-12-04 23:05:59 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -2,26 +2,26 @@ package ru.job4j.checkstyle;
 
 public class Broken {
 
-    String name;
+    private String name;
 
-    Broken() {
-    }
-
-    void echo() {
-    }
-
-    void media(Object obj) {
-        if (obj != null) {
-            System.out.println(obj);
-        }
-    }
-
-    void method(int a, int b, int c, int d, int e, int f, int g) {
-    }
+    private String surname;
 
     private int sizeOfEmpty = 10;
 
     public static final String NEW_VALUE = "";
 
-    public String surname;
+    public Broken() {
+    }
+
+    public void echo() {
+    }
+
+    public void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    public void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
 }
