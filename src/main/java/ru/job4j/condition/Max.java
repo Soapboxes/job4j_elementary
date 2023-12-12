eda6db7c8a1fe508c543c1facf5d2c758e2a3590
Soapboxes <furmaks@gmail.com>
2023-12-12 23:19:46 +0300

    3.3. Фиксация изменений на github [#243564]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -11,10 +11,15 @@ public class Max {
         return max(max(left, right), max(first, second));
     }
 
+    public static int max(int left, int right, int up) {
+        return max(left, max(right, up));
+    }
+
     public static void main(String[] args) {
         Max max = new Max();
         Max max1 = new Max();
         System.out.println(max.max(1, 2));
         System.out.println(max1.max(1, 2, 3, 4));
+        System.out.println(max(1, 2, 3));
     }
 }
